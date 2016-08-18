3a598eab579344510895665aee0c74495026ebb7
Sven Luijten <svenluijten@outlook.com>
2016-08-18 23:42:04 +0200

    test multiple levels of dot notation

--- tests/CreateNewViewsTest.php
+++ tests/CreateNewViewsTest.php
@@ -18,9 +18,14 @@ class CreateNewViewsTest extends TestCase
     public function it_dynamically_creates_subfolders()
     {
         $this->view->create('pages.index');
+        $this->view->create('pages.about.index');
 
         $this->assertTrue(
             $this->filesystem->has('pages/index.blade.php')
+        );
+
+        $this->assertTrue(
+            $this->filesystem->has('pages/about/index.blade.php')
         );
     }
 }
