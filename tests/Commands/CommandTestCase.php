a39c7c94b6cf87e55de1a1e1c2274ccb804b1769
Sven Luijten <svenluijten1995@gmail.com>
2016-09-02 20:32:45 +0200

    theoretical work on commands

---
+++ tests/Commands/CommandTestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Sven\ArtisanView\Tests\Commands;
+
+use Sven\ArtisanView\Tests\TestCase;
+
+abstract class CommandTestCase extends TestCase
+{
+    public function setUp()
+    {
+        //
+    }
+}
---
+++ tests/Commands/MakeViewTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Sven\ArtisanView\Tests\Commands;
+
+use Sven\ArtisanView\Tests\TestCase;
+
+class MakeViewTest extends CommandTestCase
+{
+    /** @test */
+    public function it_creates_a_view()
+    {
+
+    }
+}
