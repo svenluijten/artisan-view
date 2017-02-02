fded9bf3d9b7d02608dd5a7186c6c35b264a3c85
sasin91 <jonas.kerwin.hansen@gmail.com>
2017-02-02 08:51:33 +0100

    Add support for Laravel 5.4 (#20)
    * Update ArtisanViewServiceProvider.php
    
    shared & bindShared should be replaceable by call to app->singleton.
    But since it's only commands getting registered, 
    then making it a deferred ServiceProvider shouldn't be an issue. :)
    
    * cs

--- src/ArtisanViewServiceProvider.php
+++ src/ArtisanViewServiceProvider.php
@@ -7,24 +7,20 @@ use Illuminate\Support\ServiceProvider;
 class ArtisanViewServiceProvider extends ServiceProvider
 {
     /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
      * Bootstrap the application services.
      *
      * @return void
      */
     public function boot()
     {
-        $this->app['make:view'] = $this->app->share(function () {
-            return new Commands\MakeViewCommand();
-        });
-
-        $this->app['scrap:view'] = $this->app->share(function () {
-            return new Commands\ScrapViewCommand();
-        });
-
-        $this->commands(
-            'make:view',
-            'scrap:view'
-        );
+        //
     }
 
     /**
@@ -34,6 +30,22 @@ class ArtisanViewServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->commands([
+            Commands\MakeViewCommand::class,
+            Commands\ScrapViewCommand::class,
+        ]);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            Commands\MakeViewCommand::class,
+            Commands\ScrapViewCommand::class,
+        ];
     }
 }
