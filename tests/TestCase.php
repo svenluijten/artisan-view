05bbe0764f66ecf49040f2a633c1f12750a2b725
Sven Luijten <svenluijten1995@gmail.com>
2017-08-26 17:49:49 +0200

    clear out the view folder after each test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,10 @@
 
 namespace Sven\ArtisanView\Tests;
 
-use Illuminate\View\Factory;
-use Sven\ArtisanView\ServiceProvider;
 use GrahamCampbell\TestBench\AbstractPackageTestCase;
 use GrahamCampbell\TestBenchCore\ServiceProviderTrait;
+use Illuminate\View\Factory;
+use Sven\ArtisanView\ServiceProvider;
 
 abstract class TestCase extends AbstractPackageTestCase
 {
@@ -24,6 +24,44 @@ abstract class TestCase extends AbstractPackageTestCase
         parent::setUp();
 
         $this->view = $this->app->make(Factory::class);
+    }
+
+    /**
+     * Tear down the testing environment.
+     */
+    public function tearDown()
+    {
+        /** @var \Illuminate\View\FileViewFinder $viewFinder */
+        $viewFinder = app('view.finder');
+
+        foreach ($viewFinder->getPaths() as $path) {
+            $this->clearDirectory($path);
+        }
+
+        parent::tearDown();
+    }
+
+    /**
+     * @param string $path
+     */
+    protected function clearDirectory($path)
+    {
+        if (! is_dir($path)) {
+            return;
+        }
+
+        foreach (scandir($path) as $object) {
+            if (in_array($object, ['..', '.', 'welcome.blade.php', 'errors'])) {
+                continue;
+            }
+
+            if (is_dir($path.DIRECTORY_SEPARATOR.$object)) {
+                $this->clearDirectory($path.DIRECTORY_SEPARATOR.$object);
+                rmdir($path.DIRECTORY_SEPARATOR.$object);
+            } else {
+                unlink($path.DIRECTORY_SEPARATOR.$object);
+            }
+        }
     }
 
     /**
