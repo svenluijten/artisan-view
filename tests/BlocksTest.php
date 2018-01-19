8b1d92c067575c57f2caf7f63026ab73280252d5
Christopher Di Carlo <chris@dicarlosystems.ca>
2018-01-19 13:54:35 -0500

    Added options to generate sections for @yield and @push if extended
    Added tests

--- tests/BlocksTest.php
+++ tests/BlocksTest.php
@@ -4,6 +4,7 @@ namespace Sven\ArtisanView\Tests;
 
 use Sven\ArtisanView\Blocks\Extend;
 use Sven\ArtisanView\Blocks\InlineSection;
+use Sven\ArtisanView\Blocks\Push;
 use Sven\ArtisanView\Blocks\Section;
 
 class BlocksTest extends TestCase
@@ -37,6 +38,17 @@ class BlocksTest extends TestCase
 
         $this->assertEquals(
             "@section('title', 'Some title')".PHP_EOL.PHP_EOL,
+            $block->render()
+        );
+    }
+
+    /** @test */
+    public function it_renders_a_push_block()
+    {
+        $block = new Push('after-scripts');
+
+        $this->assertEquals(
+            "@push('after-scripts')".PHP_EOL.PHP_EOL.'@endpush'.PHP_EOL.PHP_EOL,
             $block->render()
         );
     }
