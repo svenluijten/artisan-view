7c30e76537d4077c1d95faebf6314ceea0234ce7
Sven Luijten <svenluijten@users.noreply.github.com>
2018-10-07 22:29:38 +0200

    remove trait from base TestCase class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,12 +5,10 @@ namespace Sven\ArtisanView\Tests;
 use GrahamCampbell\TestBench\AbstractPackageTestCase;
 use GrahamCampbell\TestBenchCore\ServiceProviderTrait;
 use Sven\ArtisanView\ServiceProvider;
-use Sven\LaravelTestingUtils\InteractsWithViews;
 
 abstract class TestCase extends AbstractPackageTestCase
 {
     use ServiceProviderTrait;
-    use InteractsWithViews;
     use InteractsWithConsole;
 
     /**
