e6ff6f212cb27357a127d0d6a64313f7f1be1a8d
Sven Luijten <svenluijten1995@gmail.com>
2019-09-01 13:37:08 +0000

    Apply fixes from StyleCI

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -10,7 +10,7 @@ class ServiceProvider extends LaravelServiceProvider
 
     public function register(): void
     {
-        if (!$this->app->runningInConsole()) {
+        if (! $this->app->runningInConsole()) {
             return;
         }
 
