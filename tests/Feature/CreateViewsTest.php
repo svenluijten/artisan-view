cdf44e90d5e26dee0595e47b9ab29ed1a2d96311
Sven Luijten <svenluijten1995@gmail.com>
2019-12-21 21:54:04 +0100

    Please phpstan
    The `unset($command);` calls are there because of Laravel's
    `__destruct`-magic they employ on the `PendingCommand` class. We need to
    make sure the command we call is actually run, so we need to manually
    make sure the destructor is executed.

--- tests/Feature/CreateViewsTest.php
+++ tests/Feature/CreateViewsTest.php
@@ -13,8 +13,11 @@ class CreateViewsTest extends TestCase
     /** @test */
     public function it_creates_a_view(): void
     {
-        $this->artisan(MakeView::class, ['name' => 'testing'])
-            ->assertExitCode(0);
+        /** @var \Illuminate\Foundation\Testing\PendingCommand $command */
+        $command = $this->artisan(MakeView::class, ['name' => 'testing']);
+
+        $command->assertExitCode(0);
+        unset($command);
 
         $this->assertViewExists('testing');
     }
@@ -22,8 +25,11 @@ class CreateViewsTest extends TestCase
     /** @test */
     public function it_makes_a_view_in_a_nested_folder(): void
     {
-        $this->artisan(MakeView::class, ['name' => 'foo.bar'])
-            ->assertExitCode(0);
+        /** @var \Illuminate\Foundation\Testing\PendingCommand $command */
+        $command = $this->artisan(MakeView::class, ['name' => 'foo.bar']);
+
+        $command->assertExitCode(0);
+        unset($command);
 
         $this->assertViewExists('foo.bar');
         $this->assertDirectoryExists(__DIR__.'/../resources/views/foo');
@@ -32,10 +38,14 @@ class CreateViewsTest extends TestCase
     /** @test */
     public function it_creates_a_view_with_a_different_extension(): void
     {
-        $this->artisan(MakeView::class, [
+        /** @var \Illuminate\Foundation\Testing\PendingCommand $command */
+        $command = $this->artisan(MakeView::class, [
             'name' => 'test',
             '--extension' => 'html.twig',
-        ])->assertExitCode(0);
+        ]);
+
+        $command->assertExitCode(0);
+        unset($command);
 
         $this->assertFileExists(__DIR__.'/../resources/views/test.html.twig');
     }
@@ -43,10 +53,14 @@ class CreateViewsTest extends TestCase
     /** @test */
     public function it_creates_all_four_restful_resource_views(): void
     {
-        $this->artisan(MakeView::class, [
+        /** @var \Illuminate\Foundation\Testing\PendingCommand $command */
+        $command = $this->artisan(MakeView::class, [
             'name' => 'posts',
             '--resource' => true,
-        ])->assertExitCode(0);
+        ]);
+
+        $command->assertExitCode(0);
+        unset($command);
 
         $this->assertDirectoryExists(__DIR__.'/../resources/views/posts');
         $this->assertViewExists('posts.index');
@@ -58,10 +72,14 @@ class CreateViewsTest extends TestCase
     /** @test */
     public function it_creates_a_resource_in_a_nested_folder(): void
     {
-        $this->artisan(MakeView::class, [
+        /** @var \Illuminate\Foundation\Testing\PendingCommand $command */
+        $command = $this->artisan(MakeView::class, [
             'name' => 'admin.posts',
             '--resource' => true,
-        ])->assertExitCode(0);
+        ]);
+
+        $command->assertExitCode(0);
+        unset($command);
 
         $this->assertViewExists('admin.posts.index');
         $this->assertViewExists('admin.posts.show');
@@ -72,11 +90,15 @@ class CreateViewsTest extends TestCase
     /** @test */
     public function it_creates_part_of_a_restful_resource(): void
     {
-        $this->artisan(MakeView::class, [
+        /** @var \Illuminate\Foundation\Testing\PendingCommand $command */
+        $command = $this->artisan(MakeView::class, [
             'name' => 'posts',
             '--resource' => true,
             '--verb' => ['index', 'show'],
-        ])->assertExitCode(0);
+        ]);
+
+        $command->assertExitCode(0);
+        unset($command);
 
         $this->assertViewExists('posts.index');
         $this->assertViewExists('posts.show');
@@ -87,11 +109,15 @@ class CreateViewsTest extends TestCase
     /** @test */
     public function it_creates_a_resource_with_a_custom_extension(): void
     {
-        $this->artisan(MakeView::class, [
+        /** @var \Illuminate\Foundation\Testing\PendingCommand $command */
+        $command = $this->artisan(MakeView::class, [
             'name' => 'posts',
             '--resource' => true,
             '--extension' => 'html.twig',
-        ])->assertExitCode(0);
+        ]);
+
+        $command->assertExitCode(0);
+        unset($command);
 
         $this->assertFileExists(__DIR__.'/../resources/views/posts/index.html.twig');
         $this->assertFileExists(__DIR__.'/../resources/views/posts/show.html.twig');
--- tests/Feature/FillViewsTest.php
+++ tests/Feature/FillViewsTest.php
@@ -13,10 +13,14 @@ class FillViewsTest extends TestCase
     /** @test */
     public function it_adds_the_extends_tag_to_the_generated_view(): void
     {
-        $this->artisan(MakeView::class, [
+        /** @var \Illuminate\Foundation\Testing\PendingCommand $command */
+        $command = $this->artisan(MakeView::class, [
             'name' => 'index',
             '--extends' => 'layouts.master',
-        ])->assertExitCode(0);
+        ]);
+
+        $command->assertExitCode(0);
+        unset($command);
 
         $this->assertViewExists('index');
         $this->assertViewEquals("@extends('layouts.master')".PHP_EOL.PHP_EOL, 'index');
@@ -25,10 +29,14 @@ class FillViewsTest extends TestCase
     /** @test */
     public function it_adds_a_section_to_the_generated_view(): void
     {
-        $this->artisan(MakeView::class, [
+        /** @var \Illuminate\Foundation\Testing\PendingCommand $command */
+        $command = $this->artisan(MakeView::class, [
             'name' => 'index',
             '--section' => ['content'],
-        ])->assertExitCode(0);
+        ]);
+
+        $command->assertExitCode(0);
+        unset($command);
 
         $this->assertViewEquals("@section('content')".PHP_EOL.PHP_EOL.'@endsection'.PHP_EOL.PHP_EOL, 'index');
     }
@@ -36,10 +44,14 @@ class FillViewsTest extends TestCase
     /** @test */
     public function it_adds_multiple_sections_to_the_generated_view(): void
     {
-        $this->artisan(MakeView::class, [
+        /** @var \Illuminate\Foundation\Testing\PendingCommand $command */
+        $command = $this->artisan(MakeView::class, [
             'name' => 'index',
             '--section' => ['content', 'scripts'],
-        ])->assertExitCode(0);
+        ]);
+
+        $command->assertExitCode(0);
+        unset($command);
 
         $contentSection = "@section('content')".PHP_EOL.PHP_EOL.'@endsection'.PHP_EOL.PHP_EOL;
         $scriptSection = "@section('scripts')".PHP_EOL.PHP_EOL.'@endsection'.PHP_EOL.PHP_EOL;
@@ -50,10 +62,14 @@ class FillViewsTest extends TestCase
     /** @test */
     public function it_adds_an_inline_section_to_the_generated_view(): void
     {
-        $this->artisan(MakeView::class, [
+        /** @var \Illuminate\Foundation\Testing\PendingCommand $command */
+        $command = $this->artisan(MakeView::class, [
             'name' => 'index',
             '--section' => ['title:My Title'],
-        ])->assertExitCode(0);
+        ]);
+
+        $command->assertExitCode(0);
+        unset($command);
 
         $this->assertViewEquals("@section('title', 'My Title')".PHP_EOL.PHP_EOL, 'index');
     }
@@ -61,10 +77,14 @@ class FillViewsTest extends TestCase
     /** @test */
     public function it_adds_a_normal_an_an_inline_section_to_the_generated_view(): void
     {
-        $this->artisan(MakeView::class, [
+        /** @var \Illuminate\Foundation\Testing\PendingCommand $command */
+        $command = $this->artisan(MakeView::class, [
             'name' => 'index',
             '--section' => ['content', 'title:My Title'],
-        ])->assertExitCode(0);
+        ]);
+
+        $command->assertExitCode(0);
+        unset($command);
 
         $contentSection = "@section('content')".PHP_EOL.PHP_EOL.'@endsection'.PHP_EOL.PHP_EOL;
 
